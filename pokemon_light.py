b02f4113ece0a28672c9e4859819907a134b9bbb
Naim Momin <mominnaim11@gmail.com>
2023-08-07 14:44:53 -0500

    Now you can pick your attack as well as pick your pokemon, Round 8 is boss fight.

--- pokemon_light.py
+++ pokemon_light.py
@@ -42,7 +42,7 @@ class game_engine(object):
 
         rounds = 1
 
-        while rounds != 7:
+        while rounds != 8:
             print(f'this is round {rounds}\n\n')
 
             if rounds in [1, 3, 5, 7]:
@@ -104,7 +104,7 @@ class game_engine(object):
 
                 level_one_pokemon = [wild_abra, wild_bulbasaur, wild_charmander, wild_chimchar, wild_squirtle]
                 opponent_pokemon = random.choice(level_one_pokemon)
-                self.battle_arena(wild_charmander)
+                self.battle_arena(opponent_pokemon)
             elif rounds == 4:
                 wild_charmeleon = Charmeleon("wild charmeleon", 200)
                 wild_ivysaur = Ivysaur("wild ivysaur", 200)
@@ -114,7 +114,7 @@ class game_engine(object):
 
                 level_two_pokemon = [wild_charmeleon, wild_kadabra, wild_ivysaur, wild_monferno, wild_wortortle]
                 opponent_pokemon = random.choice(level_two_pokemon)
-                self.battle_arena(wild_charmeleon)
+                self.battle_arena(opponent_pokemon)
             elif rounds == 6:
                 wild_alakazam = Alakazam("a wild Alakazam", 300)
                 wild_venusaur = Venusaur("a wild Venusaur", 300)
@@ -125,7 +125,8 @@ class game_engine(object):
                 opponent_pokemon = random.choice(level_three_pokemon)
                 self.battle_arena(opponent_pokemon)
             elif rounds == 8:
-                pass
+                print("Get ready to fight the boss MEWTWO!!!")
+                self.battle_arena(mewtwo)
 
             rounds = rounds + 1
 
@@ -162,8 +163,7 @@ class game_engine(object):
 
             if pokemon_2.hp <= 0:
                 print(f"{pokemon_2.name} has lost, and you win the game")
-                winning_evolution = pokemon_1.name
-                self.player.evolve_pokemon(winning_evolution)
+                self.player.evolve_pokemon(pokemon_1.name)
                 return True
             else:
                 print(f"{pokemon_2.name} has {pokemon_2.hp} health left\n")
@@ -180,7 +180,7 @@ class game_engine(object):
                 self.player.backpack.remove(pokemon_1)
                 if len(self.player.backpack) != 0:
                     number = 1
-                    for i in (self.player.backpack):
+                    for i in self.player.backpack:
                         print(f'{number} -> {i.name}')
                         number = number + 1
                     battling_pokemon = input("\n=> ")
@@ -216,22 +216,20 @@ class Player(object):
         self.backpack.append(pokemon)
 
     def evolve_pokemon(self, evolved_pokemon):
-        if evolved_pokemon == "charmander":
+        if evolved_pokemon == "charmander" or evolved_pokemon == "Charmander":
             for i, poke in enumerate(self.backpack):
                 if poke.name == "charmander":
                     self.backpack.pop(i)
                     charmeleon = Charmeleon("charmeleon", 200)
                     self.backpack.insert(i, charmeleon)
-                else:
-                    print("This pokemon is not in your backpack")
+                    break
         elif evolved_pokemon == "charmeleon":
             for i, poke in enumerate(self.backpack):
                 if poke.name == "charmeleon":
                     self.backpack.pop(i)
                     charizard = Charizard("charizard", 300)
                     self.backpack.insert(i, charizard)
-                else:
-                    print("This pokemon is not in your backpack")
+                    break
         elif evolved_pokemon == "chimchar":
             for i, poke in enumerate(self.backpack):
                 if poke.name == "chimchar":
@@ -306,15 +304,13 @@ class Pokemon:
 class Charizard(Pokemon):
     def __init__(self, name, hp):
         super().__init__(name, hp)
-        self.attack = {"Crimson strike": self.crimson_strike(), "Fire breathe": self.fire_breathe()}
+        self.attacking = {"Crimson strike": self.crimson_strike, "Fire breathe": self.fire_breathe}
 
     def attack(self):
-        list_of_attacks = [self.crimson_strike(), self.fire_breathe()]
+        list_of_attacks = [self.crimson_strike, self.fire_breathe]
         random_attack = random.choice(list_of_attacks)
-        if random_attack == self.crimson_strike():
-            return self.crimson_strike()
-        elif random_attack == self.fire_breathe():
-            return self.fire_breathe()
+        damage = random_attack()
+        return damage
 
     def crimson_strike(self):
         print("Charizard uses Crimson Strike!")
@@ -328,7 +324,7 @@ class Charizard(Pokemon):
 
     def use_attack(self):
         i = 1
-        for key, value in self.attack.items():
+        for key, value in self.attacking.items():
             print(f'{i} --> {key}')
             i = i + 1
 
@@ -343,15 +339,14 @@ class Charizard(Pokemon):
 class Charmeleon(Pokemon):
     def __init__(self, name, hp):
         super().__init__(name, hp)
-        self.attacking = {"Fire tail": self.fire_tail(), "Fire scratch": self.fire_scratch()}
+        self.attacking = {"Fire tail": self.fire_tail, "Fire scratch": self.fire_scratch}
 
     def attack(self):
-        list_of_attacks = [self.fire_tail(), self.fire_scratch()]
+        list_of_attacks = [self.fire_tail, self.fire_scratch]
         random_attack = random.choice(list_of_attacks)
-        if random_attack == self.fire_tail():
-            return self.fire_tail()
-        elif random_attack == self.fire_scratch():
-            return self.fire_scratch()
+        damage = random_attack()
+        return damage
+
 
     def fire_tail(self):
         print("Charmeleon uses Fire Tail!")
@@ -380,7 +375,7 @@ class Charmeleon(Pokemon):
 class Charmander(Pokemon):
     def __init__(self, name, hp):
         super().__init__(name, hp)
-        self.attacking = {"Ember": self.ember()}
+        self.attacking = {"Ember": self.ember}
 
     def attack(self):
         print("Charmander uses Ember!")
@@ -407,109 +402,391 @@ class Charmander(Pokemon):
 class Chimchar(Pokemon):
     def __init__(self, name, hp):
         super().__init__(name, hp)
+        self.attacking = {"Ember": self.ember}
 
     def attack(self):
+        print("Chimchar uses Ember!")
         damage = 30
         return damage
+
+    def ember(self):
+        print("Charmander uses Ember!")
+        damage = 30
+        return damage
+
+    def use_attack(self):
+        i = 1
+        for key, value in self.attacking.items():
+            print(f'{i} --> {key}')
+            i = i + 1
+
+        pick_attack = input(f'Which attack would you like to use \nPick a number =>')
+
+        if pick_attack == "1":
+            pass
+        if pick_attack == "2":
+            pass
 
 
 class Monferno(Pokemon):
     def __init__(self, name, hp):
         super().__init__(name, hp)
+        self.attacking = {"Fire Punch": self.fire_punch, "Fire Slap": self.fire_slap}
 
     def attack(self):
+        list_of_attacks = [self.fire_slap, self.fire_punch]
+        random_attack = random.choice(list_of_attacks)
+        damage = random_attack()
+        return damage
+
+    def fire_punch(self):
+        print("Monferno uses Fire punch")
         damage = 50
         return damage
+
+    def fire_slap(self):
+        print("Monferno uses Fire slap")
+        damage = 50
+        return damage
+
+    def use_attack(self):
+        i = 1
+        for key, value in self.attacking.items():
+            print(f'{i} --> {key}')
+            i = i + 1
+
+        pick_attack = input(f'Which attack would you like to use \nPick a number =>')
+
+        if pick_attack == "1":
+            return self.fire_punch()
+        if pick_attack == "2":
+            return self.fire_slap()
 
 
 class Infernape(Pokemon):
     def __init__(self, name, hp):
         super().__init__(name, hp)
+        self.attacking = {"Volcanic Punch": self.volcanic_punch, "Lava Rain": self.lava_rain}
 
     def attack(self):
-        damage = 70
+        list_of_attacks = [self.volcanic_punch, self.lava_rain]
+        random_attack = random.choice(list_of_attacks)
+        damage = random_attack()
         return damage
+
+    def volcanic_punch(self):
+        print("Infernape uses Volcanic punch!")
+        damage = 75
+        return damage
+
+    def lava_rain(self):
+        print("Infernape uses Lava rain")
+        damage = 50
+        return damage
+
+    def use_attack(self):
+        i = 1
+        for key, value in self.attacking.items():
+            print(f'{i} --> {key}')
+            i = i + 1
+
+        pick_attack = input(f'Which attack would you like to use \nPick a number =>')
+
+        if pick_attack == "1":
+            return self.volcanic_punch()
+        if pick_attack == "2":
+            return self.lava_rain()
 
 
 class Squirtle(Pokemon):
     def __init__(self, name, hp):
         super().__init__(name, hp)
+        self.attacking = {"Water Gun": self.water_gun}
 
     def attack(self):
+        print("Squirtle uses Water gun!")
         damage = 30
         return damage
+
+    def water_gun(self):
+        print("Squirtle uses Water gun!")
+        damage = 30
+        return damage
+
+    def use_attack(self):
+        i = 1
+        for key, value in self.attacking.items():
+            print(f'{i} --> {key}')
+            i = i + 1
+
+        pick_attack = input(f'Which attack would you like to use \nPick a number =>')
+
+        if pick_attack == "1":
+            return self.water_gun()
 
 
 class Wartortle(Pokemon):
     def __init__(self, name, hp):
         super().__init__(name, hp)
+        self.attacking = {"Water Pump": self.waterfall, "Waterfall": self.water_pump}
 
     def attack(self):
+        list_of_attacks = [self.water_pump, self.waterfall]
+        random_attack = random.choice(list_of_attacks)
+        damage = random_attack()
+        return damage
+
+    def waterfall(self):
+        print("Wartortle uses Water fall!")
+        damage = 50
+        return 50
+
+    def water_pump(self):
+        print("Wartortle uses Water pump!")
         damage = 50
         return damage
+
+    def use_attack(self):
+        i = 1
+        for key, value in self.attacking.items():
+            print(f'{i} --> {key}')
+            i = i + 1
+
+        pick_attack = input(f'Which attack would you like to use \nPick a number =>')
+
+        if pick_attack == "1":
+            return self.waterfall()
+        if pick_attack == "2":
+            return self.water_pump()
 
 
 class Blastoise(Pokemon):
     def __init__(self, name, hp):
         super().__init__(name, hp)
+        self.attacking = {"Hydro Pump": self.hydro_pump, "Tsunami": self.tsunami}
 
     def attack(self):
-        damage = 70
+        list_of_attacks = [self.hydro_pump, self.tsunami]
+        random_attack = random.choice(list_of_attacks)
+        damage = random_attack()
         return damage
+
+    def hydro_pump(self):
+        print("Blastoise uses Hydro pump!")
+        damage = 75
+        return damage
+
+    def tsunami(self):
+        print("Blastoise unleashes a Tsunami!")
+        damage = 90
+        return damage
+
+    def use_attack(self):
+        i = 1
+        for key, value in self.attacking.items():
+            print(f'{i} --> {key}')
+            i = i + 1
+
+        pick_attack = input(f'Which attack would you like to use \nPick a number =>')
+
+        if pick_attack == "1":
+            return self.hydro_pump()
+        if pick_attack == "2":
+            return self.tsunami()
 
 
 class Bulbasaur(Pokemon):
     def __init__(self, name, hp):
         super().__init__(name, hp)
+        self.attacking = {"Vine whip": self.vine_whip}
 
     def attack(self):
+        print("Bulbasaur uses vine whip")
         damage = 30
         return damage
+
+    def vine_whip(self):
+        print("Bulbasaur uses vine whip")
+        damage = 30
+        return damage
+
+    def use_attack(self):
+        i = 1
+        for key, value in self.attacking.items():
+            print(f'{i} --> {key}')
+            i = i + 1
+
+        pick_attack = input(f'Which attack would you like to use \nPick a number =>')
+
+        if pick_attack == "1":
+            return self.vine_whip()
 
 
 class Ivysaur(Pokemon):
     def __init__(self, name, hp):
         super().__init__(name, hp)
+        self.attacking = {"Pollen Power": self.pollen_power, "Branch Slap": self.branch_slap}
 
     def attack(self):
-        damage = 50
+        list_of_attacks = [self.branch_slap, self.pollen_power]
+        random_attack = random.choice(list_of_attacks)
+        damage = random_attack()
         return damage
+
+    def pollen_power(self):
+        print("Ivysaur uses Pollen Powder")
+        damage = 30
+        return damage
+
+    def branch_slap(self):
+        print("Ivysaur uses branch slap")
+        damage = 30
+        return damage
+
+    def use_attack(self):
+        i = 1
+        for key, value in self.attacking.items():
+            print(f'{i} --> {key}')
+            i = i + 1
+
+        pick_attack = input(f'Which attack would you like to use \nPick a number =>')
+
+        if pick_attack == "1":
+            return self.pollen_power()
+        if pick_attack == "2":
+            return self.branch_slap()
 
 
 class Venusaur(Pokemon):
     def __init__(self, name, hp):
         super().__init__(name, hp)
+        self.attacking = {"Jungle Hammer": self.jungle_hammer, "Forest whip": self.forest_whip}
 
     def attack(self):
-        damage = 70
+        list_of_attacks = [self.jungle_hammer, self.forest_whip]
+        random_attack = random.choice(list_of_attacks)
+        damage = random_attack()
         return damage
+
+    def forest_whip(self):
+        print("Venusaur uses Forest whip!")
+        damage = 60
+        return damage
+
+    def jungle_hammer(self):
+        print("Venusaur uses Jungle hammer!")
+        damage = 75
+        return damage
+
+    def use_attack(self):
+        i = 1
+        for key, value in self.attacking.items():
+            print(f'{i} --> {key}')
+            i = i + 1
+
+        pick_attack = input(f'Which attack would you like to use \nPick a number =>')
+
+        if pick_attack == "1":
+            return self.jungle_hammer()
+        if pick_attack == "2":
+            return self.forest_whip()
 
 
 class Abra(Pokemon):
     def __init__(self, name, hp):
         super().__init__(name, hp)
+        self.attacking = {"Slap": self.slap}
 
     def attack(self):
+        print("Abra uses slap!")
         damage = 30
         return damage
+
+    def slap(self):
+        print("Abra uses slap!")
+        damage = 30
+        return damage
+
+    def use_attack(self):
+        i = 1
+        for key, value in self.attacking.items():
+            print(f'{i} --> {key}')
+            i = i + 1
+
+        pick_attack = input(f'Which attack would you like to use \nPick a number =>')
+
+        if pick_attack == "1":
+            self.slap()
 
 
 class Kadabra(Pokemon):
     def __init__(self, name, hp):
         super().__init__(name, hp)
+        self.attacking = {"Psychic Slap": self.psychic_slap, "Mind Twist": self.mind_twist}
 
     def attack(self):
+        list_of_attacks = [self.psychic_slap, self.mind_twist]
+        random_attack = random.choice(list_of_attacks)
+        damage = random_attack()
+        return damage
+
+    def psychic_slap(self):
+        print("Kadabra uses Psychic Slap!")
         damage = 50
         return damage
+
+    def mind_twist(self):
+        print("Kadabra uses Mind twist")
+        damage = 50
+        return damage
+
+    def use_attack(self):
+        i = 1
+        for key, value in self.attacking.items():
+            print(f'{i} --> {key}')
+            i = i + 1
+
+        pick_attack = input(f'Which attack would you like to use \nPick a number =>')
+
+        if pick_attack == "1":
+            return self.psychic_slap()
+        if pick_attack == "2":
+            return self.mind_twist()
 
 
 class Alakazam(Pokemon):
     def __init__(self, name, hp):
         super().__init__(name, hp)
+        self.attacking = {"Power of Zen": self.power_of_zen, "Mantra Kick": self.mantra_kick}
 
     def attack(self):
-        damage = 70
+        list_of_attacks = [self.power_of_zen, self.mantra_kick]
+        random_attack = random.choice(list_of_attacks)
+        damage = random_attack()
         return damage
+
+    def power_of_zen(self):
+        print("Alakazam uses the Power of Zen")
+        damage = 50
+        return damage
+
+    def mantra_kick(self):
+        print("Alakazam uses Mantra Kick")
+        damage = 75
+        return damage
+
+    def use_attack(self):
+        i = 1
+        for key, value in self.attacking.items():
+            print(f'{i} --> {key}')
+            i = i + 1
+
+        pick_attack = input(f'Which attack would you like to use \nPick a number =>')
+
+        if pick_attack == "1":
+            return self.power_of_zen()
+        if pick_attack == "2":
+            return self.mantra_kick()
 
 
 class Mewtwo(Pokemon):
@@ -517,7 +794,24 @@ class Mewtwo(Pokemon):
         super().__init__(name, hp)
 
     def attack(self):
-        damage = 50
+        list_of_attacks = [self.pulse_ray, self.major_beatdown, self.tail_shock]
+        random_attack = random.choice(list_of_attacks)
+        damage = random_attack()
+        return damage
+
+    def pulse_ray(self):
+        print("Mewtwo uses Pulse Ray")
+        damage = 75
+        return damage
+
+    def major_beatdown(self):
+        print("Mewtwo uses Major beatdown")
+        damage = 60
+        return damage
+
+    def tail_shock(self):
+        print("Mewtwo uses Tail shock")
+        damage = 85
         return damage
 
     def get_name(self):
